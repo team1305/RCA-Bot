4c714a8a0e14df646878521fa579957fd70a2a5f
James Green <greenjamesag@gmail.com>
2020-02-04 19:55:00 -0500

    adds intake commands

---
+++ src/main/java/frc/robot/commands/Command_Intake_Ground.java
@@ -0,0 +1,65 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj.command.Command;
+import frc.robot.Robot;
+
+public class Command_Intake_Ground extends Command {
+  public Command_Intake_Ground() {
+    // Use requires() here to declare subsystem dependencies
+    // eg. requires(chassis);
+    requires(Robot.intake);
+    requires(Robot.hopper);
+    requires(Robot.elevator);
+    requires(Robot.shooter);
+  }
+
+  // Called just before this Command runs the first time
+  @Override
+  protected void initialize() {
+  }
+
+  // Called repeatedly when this Command is scheduled to run
+  @Override
+  protected void execute() {
+    Robot.intake.extendIntake();
+    Robot.intake.enableIntake();
+    Robot.hopper.hopperIn();
+    Robot.elevator.elevatorUp();
+    Robot.shooter.ShooterSpeed(-0.1);
+
+  }
+
+  // Make this return true when this Command no longer needs to run execute()
+  @Override
+  protected boolean isFinished() {
+    return false;
+  }
+
+  // Called once after isFinished returns true
+  @Override
+  protected void end() {
+    Robot.intake.retractIntake();
+    Robot.intake.stopIntake();
+    Robot.hopper.hopperStop();
+    Robot.elevator.elevatorStop();
+    Robot.shooter.ShooterSpeed(0);
+  }
+
+  // Called when another command which requires one or more of the same
+  // subsystems is scheduled to run
+  @Override
+  protected void interrupted() {
+    Robot.intake.retractIntake();
+    Robot.intake.stopIntake();
+    Robot.hopper.hopperStop();
+    Robot.elevator.elevatorStop();
+    Robot.shooter.ShooterSpeed(0);
+  }
+}
