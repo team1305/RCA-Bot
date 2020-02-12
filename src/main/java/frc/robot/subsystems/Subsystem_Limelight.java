aa77fa6f64878589afbfd6bcd7d4666f67c502ab
Rithika Silva <rithikasilva@outlook.com>
2020-02-11 20:59:22 -0500

    Added functions to track: thor, ty, target confimation, and distance

--- src/main/java/frc/robot/subsystems/Subsystem_Limelight.java
+++ src/main/java/frc/robot/subsystems/Subsystem_Limelight.java
@@ -9,6 +9,7 @@ package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj.command.Subsystem;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import frc.robot.Robot;
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.networktables.NetworkTableInstance;
@@ -20,6 +21,51 @@ import edu.wpi.first.networktables.NetworkTableInstance;
 public class Subsystem_Limelight extends Subsystem {
   // Put methods for controlling this subsystem
   // here. Call these from Commands.
+
+  public NetworkTable table;
+  public NetworkTableEntry tx;
+
+
+
+  public Subsystem_Limelight() {
+    table = Robot.limelight.getLimelightValues();
+  }
+
+  public double get_Tx(){
+    return table.getEntry("tx").getDouble(0.0);
+  }
+
+
+  public boolean is_Target(){
+    if (table.getEntry("tv").getDouble(0) == 1) {
+      return true;
+   } else {
+      return false;
+   }
+  }
+
+  public double get_Thor(){
+    return table.getEntry("thor").getDouble(0.0);
+  }
+
+  public double get_Ty(){
+    return table.getEntry("ty").getDouble(0.0);
+  }
+
+
+  public double getDistance(){
+    double distance = 0;
+    double height_of_limelight = 26; //Inches
+    double height_of_target = 185; //Inches
+    double angle_of_limelight = 45; // Degrees
+
+    if (is_Target()){
+      distance = (height_of_target - height_of_limelight) / Math.tan(angle_of_limelight + get_Ty());
+
+    }
+    return distance;
+  }
+  
 
   @Override
   public void initDefaultCommand() {
@@ -46,18 +92,7 @@ public class Subsystem_Limelight extends Subsystem {
 
   }
 
-  public void trackInitiationLine() {
- 
+
   
-  }
 
-
-  public void trackBackTrench() {
-
-
-  }
-
-  public void trackPole() {
-
-  }
 }
