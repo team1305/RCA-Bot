a2e9019bb3547cdcce128bb0af4c3429376b6f50
Rithika Silva <rithikasilva@outlook.com>
2020-02-14 19:05:14 -0500

    Reverted to old drive code

--- src/main/java/frc/robot/subsystems/Subsystem_Drive.java
+++ src/main/java/frc/robot/subsystems/Subsystem_Drive.java
@@ -32,7 +32,7 @@ import edu.wpi.first.wpilibj.geometry.Rotation2d;
 import edu.wpi.first.wpilibj.kinematics.DifferentialDriveOdometry;
 import edu.wpi.first.wpilibj.kinematics.DifferentialDriveWheelSpeeds;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
+//import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 import frc.robot.Robot;
 import com.kauailabs.navx.frc.AHRS;
@@ -42,7 +42,7 @@ import com.kauailabs.navx.frc.AHRS;
 /**
  * Add your docs here.
  */
-public class Subsystem_Drive extends SubsystemBase {
+public class Subsystem_Drive extends Subsystem {
 
   //variable for deadband
   public double dDeadband = 0.1;
@@ -58,7 +58,7 @@ public class Subsystem_Drive extends SubsystemBase {
   private final WPI_TalonFX mtRight1 = RobotMap.mtDriveRight1;
   private final WPI_TalonFX mtRight2 = RobotMap.mtDriveRight2;
 
-  private AHRS navx = new AHRS(SerialPort.Port.kMXP);
+  //private AHRS navx = new AHRS(SerialPort.Port.kMXP);
 
   //grabs drive encoder information from RobotMap
   //private final CANEncoder enLeft1 = RobotMap.enDriveLeft1;
@@ -83,9 +83,6 @@ public class Subsystem_Drive extends SubsystemBase {
   // Internal subsystem parts, declares left and right motor groups used by differential drive
   DifferentialDrive drRobotDrive = new DifferentialDrive(scgLeft, scgRight);
 
-  // Subsystem State Value
-  //private Double enLeftStart;
-  //private Double enRightStart;
   
   //sets ramprate of drive motors -- now does things!
   public Subsystem_Drive() {
@@ -134,56 +131,40 @@ public class Subsystem_Drive extends SubsystemBase {
     return currentSpeed;
   }
 
-
-  /*
   @Override
   public void initDefaultCommand() {
     //unless interupted the default command will allow driver to drive with joystick
     setDefaultCommand(new Command_Drive_With_Joystick());
   }
-  */
-
-
-
-  @Override
-  public void periodic() {
-    // This method will be called once per scheduler run
-    //odometry.update(Rotation2d.fromDegrees(getHeading()), getLeftDistance(), getRightDistance());
-    //SmartDashboard.putNumber("Left Distance", getLeftDistance());
-    //SmartDashboard.putNumber("Right Distance", getRightDistance());
-    //SmartDashboard.putNumber("Heading", getHeading());
-    driveWithJoystick(Robot.oi.getJoystickDriver());
-  }
 
   //creates a deadband for the joystick so that the robot does not spin
   // when nobody is touching the controls
-  private double JoystickDeadBand(final double input) {
+  private double JoystickDeadBand(double input) {
     if(Math.abs(input) < dDeadband) return 0;
     else if(input > 0) return Math.pow(((input - dDeadband) * (1/(1-dDeadband))), dSquareFactor);
     else if(input < 0) return ((Math.pow(((Math.abs(input) - dDeadband) * (1/(1-dDeadband))), dSquareFactor)) * -1);
     else return 0;
   }
 
-  private double ThrottleScale(final double throttle,final double input) {
+  private double ThrottleScale(double throttle,double input) {
 		return (JoystickDeadBand(input) * (1-(throttle*dThrottleFactor)));
 	}
 
   //creates a driving function using specified joystick
-  public void driveWithJoystick(final Joystick stick) {
+  public void driveWithJoystick(Joystick stick) {
 
     //creates variables for joystick x and y values
-
-    ////TRYING drRobotDrive.setDeadband(0.09); ABOVE??? DOES IT WORK???
-    ////final double zRotation = ThrottleScale(Math.abs(stick.getY()* 1), stick.getRawAxis(4)* -1);
-    ////final double xSpeed = JoystickDeadBand(stick.getY()* 1);
-
-    final double zRotation = stick.getRawAxis(4)* -1;
-    final double xSpeed = stick.getY()* 1;
+    double zRotation = ThrottleScale(Math.abs(stick.getY()* 1), stick.getRawAxis(4)* -1);
+    double xSpeed = JoystickDeadBand(stick.getY()* 1);
 
     //uses joystick to do driving thing
     drRobotDrive.curvatureDrive(xSpeed, zRotation, true);
 
+    // double dSquareFactor2 = SmartDashboard.getNumber("dSquareFactor", dSquareFactor);
+    // double dThrottleFactor2 = SmartDashboard.getNumber("dThrottleFactor", dThrottleFactor);
 
+    // if((dSquareFactor2 != dSquareFactor)) {dSquareFactor = dSquareFactor2;}
+    // if((dThrottleFactor2 != dThrottleFactor)) {dThrottleFactor = dThrottleFactor2;}
   }
 
   //stops the drive train
@@ -191,7 +172,7 @@ public class Subsystem_Drive extends SubsystemBase {
     drRobotDrive.arcadeDrive(0, 0);
   }
 
-  public void DriveTank(final double leftValue, final double rightValue) {
+  public void DriveTank(double leftValue, double rightValue) {
     drRobotDrive.tankDrive(leftValue, rightValue);
   }
 
@@ -224,94 +205,5 @@ public class Subsystem_Drive extends SubsystemBase {
     SmartDashboard.putBoolean("Gear", bIsLow);
   }
 
-
-  /*
-  public Pose2d getPose() {
-    return odometry.getPoseMeters();
-  }
-
-  public DifferentialDriveWheelSpeeds getWheelSpeeds() {
-    return new DifferentialDriveWheelSpeeds(getLeftEncRate(), getRightEncRate());
-  }
-  */
-
-  public double getLeftEncRate() {
-    //return leftDriveEnc.getVelocity() / 60 * Constants.FEET_PER_ROTATIONS;
-    return mtLeft1.getSelectedSensorVelocity() / 60 * Constants.FEET_PER_ROTATIONS;
-  }
-
-  public double getRightEncRate() {
-    return mtRight1.getSelectedSensorVelocity() / 60 * Constants.FEET_PER_ROTATIONS;
-  }
-
-  public double getLeftEncPos() {
-    return mtLeft1.getSelectedSensorPosition();
-  }
-
-  public double getRightEncPos() {
-    return mtRight1.getSelectedSensorPosition();
-  }
-
-  /*
-  public void voltageDrive(double leftVolts, double rightVolts) {
-    mtLeft1.setVoltage(leftVolts);
-    mtRight1.setVoltage(-rightVolts);
-  }
-  */
-
-  public double getHeading() {
-    return Math.IEEEremainder(navx.getAngle(), 360) * (Constants.GYRO_REVERSED ? -1.0 : 1.0);
-  }
-
-  public double getLeftDistance() { // returns distance in feet
-    return getLeftEncPos() * Constants.FEET_PER_ROTATIONS;
-  }
-
-  public double getRightDistance() {
-    return getRightEncPos() * Constants.FEET_PER_ROTATIONS;
-  }
-
-
-
-
-  //gets encoder position for the left side
-  /*
-  public Double getleftEncederValue() {
-    Double enCode1 = enLeft1.getPosition();
-    Double enCode2 = enLeft2.getPosition();
-    if (enCode1 == enCode2){
-      return enCode1 - enLeftStart;
-    }else{
-      System.out.println("Left encoder1:" + enCode1.toString() + " encoder2:" + enCode2.toString());
-      return Math.max(enCode1, enCode2) - enLeftStart;
-    }
-  }
-  */
-
-
-  /*
-  //gets the encoder position for the right side
-  public Double getRightEncederValue() {
-    //greenjamesag@gmail.com
-    Double enCode1 = enRight1.getPosition();
-    Double enCode2 = enRight2.getPosition();
-    if (Math.abs(enCode1) == Math.abs(enCode2)){
-      return enCode1 - enRightStart;
-    }else{
-      System.out.println("Left encoder1:" + enCode1.toString() + " encoder2:" + enCode2.toString());
-      return Math.max(enCode1, enCode2) - enRightStart;
-    }
-  }
-  */
-
-
-  /*
-  //resets encoders to current value
-  public Boolean resetEncoder(){
-    enLeftStart = getleftEncederValue();
-    enRightStart = getRightEncederValue();
-    return true;
-  }
-  */
 }
 
