717192a0439a637f1220311818c7ed700fa9683a
Rithika Silva <rithikasilva@outlook.com>
2020-02-16 13:41:34 -0500

    Removed SmartDashboard code

--- src/main/java/frc/robot/subsystems/Subsystem_LED.java
+++ src/main/java/frc/robot/subsystems/Subsystem_LED.java
@@ -7,9 +7,15 @@
 
 package frc.robot.subsystems;
 
+import java.awt.Color;
+import java.util.Map;
+
 import com.revrobotics.CANSparkMax;
 
 import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.shuffleboard.SuppliedValueWidget;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.RobotMap;
 
 /**
@@ -22,14 +28,26 @@ public class Subsystem_LED extends Subsystem {
   private final CANSparkMax mtled = RobotMap.mtLed;
   
 
-  private final double C_blue = 0.87;
-  private final double C_green = 0.77;
-  private final double C_red = 0.61;
-  private final double C_yellow = 0.69;
+  private final double C_blue = 0.87; //FMS
+  private final double C_green = 0.77; //FMS
+  private final double C_red = 0.61; //FMS
+  private final double C_yellow = 0.69; //FMS
   private final double C_aqua = 0.81;
-  private final double C_white = 0.93;
-  private final double C_violet = 0.91;
-  private final double C_black = 0.99;
+  private final double C_white = 0.93; //Intake
+  private final double C_violet = 0.91; //Violet
+  private final double C_black = 0.99; //Off
+
+  //boolean theArray[];
+
+
+  public Subsystem_LED(){
+    /*theArray = new boolean[4];
+    theArray[1] = false;
+    theArray[2] = false;
+    theArray[3] = false;
+    theArray[4] = false;
+*/
+  }
 
 
 
@@ -41,38 +59,77 @@ public class Subsystem_LED extends Subsystem {
 
   public void setBlue(){
     mtled.set(C_blue);
+    //SuppliedValueWidget colorWidget = Shuffleboard.getTab("SmartDashboard").addBoolean("FMSColor", () -> true);
+    //colorWidget.withProperties(Map.of("colorWhenTrue", Color.blue));
+
+    //theArray[1] = true;
+    //theArray[2] = false;
+    //theArray[3] = false;
+    //theArray[4] = false;
+    //SmartDashboard.putBooleanArray("FMS Values", theArray);
   }
 
   public void setGreen(){
     mtled.set(C_green);
+    //theArray[1] = false;
+    //theArray[2] = true;
+    //theArray[3] = false;
+    //theArray[4] = false;
+    //SmartDashboard.putBooleanArray("FMS Values", theArray);
   }
 
   public void setRed(){
     mtled.set(C_red);
+    //theArray[1] = false;
+    //theArray[2] = false;
+    //theArray[3] = true;
+    //theArray[4] = false;
+    //SmartDashboard.putBooleanArray("FMS Values", theArray);
   }
 
   public void setYellow(){
     mtled.set(C_yellow);
+    //theArray[1] = false;
+    //theArray[2] = false;
+    //theArray[3] = false;
+    //theArray[4] = true;
+    //SmartDashboard.putBooleanArray("FMS Values", theArray);
+    
   }
 
   public void setAqua(){
     mtled.set(C_aqua);
+    /*theArray[1] = false;
+    theArray[2] = false;
+    theArray[3] = false;
+    theArray[4] = false;
+    SmartDashboard.putBooleanArray("FMS Values", theArray);*/
   }
 
   public void setWhite(){
     mtled.set(C_white);
+    /*theArray[1] = false;
+    theArray[2] = false;
+    theArray[3] = false;
+    theArray[4] = false;
+    SmartDashboard.putBooleanArray("FMS Values", theArray);*/
   }
 
   public void setViolet(){
     mtled.set(C_violet);
+    /*theArray[1] = false;
+    theArray[2] = false;
+    theArray[3] = false;
+    theArray[4] = false;
+    SmartDashboard.putBooleanArray("FMS Values", theArray);*/
   }
 
   public void setBlack(){
     mtled.set(C_black);
+    /*theArray[1] = false;
+    theArray[2] = false;
+    theArray[3] = false;
+    theArray[4] = false;
+    SmartDashboard.putBooleanArray("FMS Values", theArray);*/
   }
-
-
-
-
-  
 }
