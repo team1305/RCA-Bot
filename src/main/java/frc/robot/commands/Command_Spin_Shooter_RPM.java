e6f1f97e6dc59252910fa710ee17d8265a35f10a
Rithika Silva <rithikasilva@outlook.com>
2020-02-16 13:39:02 -0500

    Added code to auto supply balls

--- src/main/java/frc/robot/commands/Command_Spin_Shooter_RPM.java
+++ src/main/java/frc/robot/commands/Command_Spin_Shooter_RPM.java
@@ -8,16 +8,19 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj.command.Command;
-
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.Robot;
 
 public class Command_Spin_Shooter_RPM extends Command {
+  double shooterRPMValue = 5000;
+  boolean BuseSmartDashboard;
 
   double RPM;
 
-  public Command_Spin_Shooter_RPM(double RPM) {
+  public Command_Spin_Shooter_RPM(double RPM, boolean useSmartDashboard) {
 
-    //requires(Robot.shooterrpm);
+    BuseSmartDashboard = useSmartDashboard;
+        //requires(Robot.shooterrpm);
     requires(Robot.shooter);
     this.RPM = RPM;
   }
@@ -26,13 +29,32 @@ public class Command_Spin_Shooter_RPM extends Command {
   // Called just before this Command runs the first time
   @Override
   protected void initialize() {
+    //Robot.shooter.getPidFromDashboard();//This gets value
+    //Robot.shooter.compareUpdatePid();//This compares and updates PID values
   }
 
   // Called repeatedly when this Command is scheduled to run
   @Override
   protected void execute() {
-
+    /*
+    if (BuseSmartDashboard){
+      double shooterRPMValue = SmartDashboard.getNumber("Shooter RPM", 5000);
+      Robot.shooter.setShooterRPM(shooterRPMValue);
+    }
+    else{
+      Robot.shooter.setShooterRPM(RPM);
+    }
+    */
     Robot.shooter.setShooterRPM(RPM);
+
+           
+    SmartDashboard.putNumber("shooterRPM", Robot.shooter.getShooterRPM() );
+           if (Robot.shooter.getShooterRPM() >= RPM) {
+              // We are at speed, Turn on feeders 
+              Robot.elevator.elevatorUp(0.3);
+              Robot.hopper.hopperOut(0.2);
+              Robot.intake.enableIntake(0.2);
+           }
 
   }
 
