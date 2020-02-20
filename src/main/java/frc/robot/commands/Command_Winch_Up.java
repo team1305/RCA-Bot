f151902473b2b4fe671f053b1f6b333131f787a7
Rithika Silva <rithikasilva@outlook.com>
2020-02-20 18:07:57 -0500

    Winch speed increased

--- src/main/java/frc/robot/commands/Command_Winch_Up.java
+++ src/main/java/frc/robot/commands/Command_Winch_Up.java
@@ -25,7 +25,7 @@ public class Command_Winch_Up extends Command {
   // Called repeatedly when this Command is scheduled to run
   @Override
   protected void execute() {
-    Robot.winch.WinchSpeed(0.3);
+    Robot.winch.WinchSpeed(0.5);
   }
 
   // Make this return true when this Command no longer needs to run execute()
